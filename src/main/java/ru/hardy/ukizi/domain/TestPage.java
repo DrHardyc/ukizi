10e46eb3b2a327eee86c25affd196b7ce4023cc1
dr_ha <drhardyc@gmail.com>
2021-03-20 23:50:34 +0300

    Нацчился переходить по страницам

---
+++ src/main/java/ru/hardy/ukizi/domain/TestPage.java
@@ -0,0 +1,12 @@
+package ru.hardy.ukizi.domain;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class TestPage extends Div {
+
+    public TestPage() {
+        setText("sdkfj;alksdfj asdlkf as;dlk");
+    }
+}
--- src/main/java/ru/hardy/ukizi/view/MainView.java
+++ src/main/java/ru/hardy/ukizi/view/MainView.java
@@ -1,7 +1,9 @@
 package ru.hardy.ukizi.view;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -10,17 +12,22 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.hardy.ukizi.component.EmployeeEdit;
 import ru.hardy.ukizi.domain.Employee;
+import ru.hardy.ukizi.domain.TestPage;
 import ru.hardy.ukizi.repo.EmployeeRepo;
 
 @Route
 public class MainView extends VerticalLayout {
     private final EmployeeRepo employeeRepo;
 
-    private Grid<Employee> grid = new Grid<>(Employee.class);
+    private final Grid<Employee> grid = new Grid<>();
+
+    private final Button newPage = new Button("Перейти на тестовую страницу");
 
     private final TextField filter = new TextField("", "Введите слово для поиска");
     private final Button addNewBtn = new Button("Добавить нового");
-    private final HorizontalLayout toolbar = new HorizontalLayout(filter, addNewBtn);
+    private final HorizontalLayout toolbar = new HorizontalLayout(filter, addNewBtn, newPage);
+
+
 
     private final EmployeeEdit editor;
 
@@ -54,7 +61,12 @@ public class MainView extends VerticalLayout {
         } else{
             grid.setItems(employeeRepo.findByName(name));
         }
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+                GridVariant.LUMO_NO_ROW_BORDERS, GridVariant.LUMO_ROW_STRIPES);
+        grid.addColumn(Employee::getFirstName).setHeader("Имя").setSortable(true);
+        grid.addColumn(Employee::getLastName).setHeader("Фамилия").setSortable(true);
+        grid.addColumn(Employee::getPatronymic).setHeader("Отчество").setSortable(true);
 
+        newPage.addClickListener( e -> UI.getCurrent().navigate(TestPage.class));
     }
-
 }
